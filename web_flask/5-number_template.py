7a59cc0e87808e1bbf18de135bdbd6e4bbb7a568
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-01 22:37:21 +0000

    type of data change

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def is_number(n):
     return ("{} is a number".format(n))
 
 
-@app.route("/number_template/<init:n>")
+@app.route("/number_template/<int:n>")
 def number_template(n):
     """ Display a HTML page if n is a number.
             H1 tag: Number: n inside the tab body
