9f89812f553a1cb8248174be89d6c57f5acd1030
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-01 22:46:37 +0000

    n is even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+    Script that start a Flask web application
+    listening on 0.0.0.0 port 5000.
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.stric_slashes = False
+
+
+@app.route("/")
+def display_hello():
+    """ Display a message. """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def display_hbnb():
+    """ Display a message. """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """ Display a message from a file. """
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """ Display a message from a file. """
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>")
+def is_number(n):
+    """ Display a message if the parameter is a number."""
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """ Display a HTML page if n is a number.
+            H1 tag: Number: n inside the tab body
+    """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("number_odd_of_even/<int:n>")
+def odd_or_even(n):
+    """
+        Display a HTML page if n is a number.
+            H1 tag: Number: n is even|odd
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
