26e0748ed1ad6e0a29fae3baf71bbc5e788be4d6
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-02 01:40:53 +0000

    Flask conection

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Script that start a Flask web application
+    listening on 0.0.0.0 port 5000.
+"""
+from flask import Flask, render_template
+from models.state import state
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """ Closes sessions. """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ List all states on  """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
