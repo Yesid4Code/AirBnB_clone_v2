cad64ba2aea8b95d04236cd336c48df9381e6b2f
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-02 01:45:07 +0000

    Change the name of the import State, on line 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@
     listening on 0.0.0.0 port 5000.
 """
 from flask import Flask, render_template
-from models.state import state
+from models.state import State
 from models import storage
 app = Flask(__name__)
 
